756602c9a4dfbf2e4608a9b9b66100de35dd50c1
RossieK <rosi.kirova96@gmail.com>
2021-03-07 15:45:43 +0200

    Add styled component to about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,9 @@
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-        <div>
+        <StyledAbout>
             <div className="description">
                 <div className="title">
                     <div className="hide">
@@ -21,8 +22,18 @@ const AboutSection = () => {
             <div className="image">
                 <img src={home1} alt="Guy with a camera" />
             </div>
-        </div>
+        </StyledAbout>
     );
-}
+};
+
+//Styled Components
+const StyledAbout = styled.div`
+min-height: 90vh;
+display: flex;
+align-items:center;
+justify-content:space-between;
+padding: 5rem 10 rem;
+color:white;
+`
 
 export default AboutSection;
