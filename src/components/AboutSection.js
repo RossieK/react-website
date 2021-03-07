705ece411c4dc21244880cbc53f09eb0432d8c9d
RossieK <rosi.kirova96@gmail.com>
2021-03-07 16:03:14 +0200

    Add more styled components to About section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,24 +4,24 @@ import styled from 'styled-components';
 const AboutSection = () => {
     return (
         <StyledAbout>
-            <div className="description">
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>true.</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have a professional team with amazing skills.</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </StyledDescription>
+            <StyledImg>
                 <img src={home1} alt="Guy with a camera" />
-            </div>
+            </StyledImg>
         </StyledAbout>
     );
 };
@@ -32,8 +32,26 @@ min-height: 90vh;
 display: flex;
 align-items:center;
 justify-content:space-between;
-padding: 5rem 10 rem;
-color:white;
-`
+padding: 5rem 10rem;
+color:white;`;
+
+const StyledDescription = styled.div`
+flex:1;
+padding-right:5rem;
+h2{
+    font-weight:lighter;
+}`;
+
+const StyledImg = styled.div`
+flex:1;
+overflow:hidden;
+img{
+    width:100%;
+    height:80vh;
+    object-fit:cover;
+}`;
+
+const StyledHide = styled.div`
+overflow:hidden;`;
 
 export default AboutSection;
