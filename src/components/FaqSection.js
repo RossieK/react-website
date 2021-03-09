77b55703a0aa3334e094810e4e0af10e3e40badf
RossieK <rosi.kirova96@gmail.com>
2021-03-09 17:49:58 +0200

    Add toggling to FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,42 +1,38 @@
 import styled from 'styled-components';
 import { StyledAbout } from '../styles';
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from 'framer-motion';
 
 const FaqSection = () => {
     return (
         <StyledFaq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payments Methods?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What product do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
+                    </div>
+                </Toggle>
+                <Toggle title="Daily Schedule?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
+                    </div>
+                </Toggle>
+                <Toggle title="Different Payments Methods?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
+                    </div>
+                </Toggle>
+                <Toggle title="What product do you offer?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem, corporis quo harum eveniet id vitae odit non sapiente tenetur?</p>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </StyledFaq>
     );
 };
