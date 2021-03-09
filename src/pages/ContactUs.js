c1aa329e4a66642db9cb19006a14677547e81c6f
RossieK <rosi.kirova96@gmail.com>
2021-03-09 18:38:40 +0200

    Add animation to contact page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,12 +1,64 @@
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+import styled from 'styled-components';
+import { StyledHide } from '../styles';
 
 const ContactUs = () => {
     return (
-        <motion.div exit="exit" variants={pageAnimation} initial="hidden" animate="show" style={{ background: "#fff" }}>
-            <h1>Our Contact</h1>
-        </motion.div>
+        <StyledContact exit="exit" variants={pageAnimation} initial="hidden" animate="show" style={{ background: "#fff" }}>
+            <StyledTitle>
+                <StyledHide>
+                    <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+                </StyledHide>
+            </StyledTitle>
+            <div>
+                <StyledHide>
+                    <StyledSocial variants={titleAnim}>
+                        <StyledCircle />
+                        <h2>Send Us A Message</h2>
+                    </StyledSocial>
+                </StyledHide>
+                <StyledHide>
+                    <StyledSocial variants={titleAnim}>
+                        <StyledCircle />
+                        <h2>Send An Email</h2>
+                    </StyledSocial>
+                </StyledHide>
+                <StyledHide>
+                    <StyledSocial variants={titleAnim}>
+                        <StyledCircle />
+                        <h2>Social Media</h2>
+                    </StyledSocial>
+                </StyledHide>
+            </div>
+        </StyledContact>
     );
 };
+
+const StyledContact = styled(motion.div)`
+padding: 5rem 10rem;
+color: #353535;
+min-height:90vh;
+`;
+
+const StyledTitle = styled.div`
+margin-bottom:4rem;
+color:black;
+`;
+
+const StyledCircle = styled.div`
+border-radius: 50%;
+width:3rem;
+height:3rem;
+background:#353535;
+`;
+
+const StyledSocial = styled(motion.div)`
+display:flex;
+align-items:center;
+h2{
+    margin:2rem;
+}
+`;
 
 export default ContactUs;
